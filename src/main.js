3a1d2379d1227dae67ffb7ddbfc16862868c4f36
jahglow <jahglow@gmail.com>
2016-11-29 10:28:26 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@
 
 import TableData from "./table-data";
 import AggregatedTable from "./aggregated-table";
-import ReportalBase from "r-reporal-base/src/reportal-base";
+import ReportalBase from "r-reportal-base";
 import AggregatedTableRowMeta from "./aggregated-table-row-meta";
 
 
